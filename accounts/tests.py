59d15ebb8189765f306a23da586309397831a690
taku072002 <taku07200@gmail.com>
2024-04-22 16:47:03 +0900

    Fixed file contents for review

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,19 +1,19 @@
 from django.contrib.auth import SESSION_KEY, get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.test import TestCase, Client
-from django.urls import path, reverse
+from django.test import TestCase
+from django.urls import reverse
 
 User = get_user_model()
 
 
 class TestSignupView(TestCase, UserCreationForm):
     def setUp(self):
-        self.url = reverse('accounts:signup')
+        self.url = reverse("accounts:signup")
 
     def test_success_get(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'accounts/signup.html')
+        self.assertTemplateUsed(response, "accounts/signup.html")
 
     def test_success_post(self):
         valid_data = {
@@ -21,16 +21,17 @@ class TestSignupView(TestCase, UserCreationForm):
             "email": "test@test.com",
             "password1": "testpassword",
             "password2": "testpassword",
-          }
+        }
         response = self.client.post(self.url, valid_data)
         self.assertRedirects(
-               response,
-               reverse("tweets:home"),
-               status_code=302,
-               target_status_code=200,
-          )
+            response,
+            reverse("tweets:home"),
+            status_code=302,
+            target_status_code=200,
+        )
         self.assertTrue(User.objects.filter(username=valid_data["username"]).exists())
         self.assertIn(SESSION_KEY, self.client.session)
+
 
 #     def test_failure_post_with_empty_form(self):
 
@@ -53,9 +54,9 @@ class TestSignupView(TestCase, UserCreationForm):
 #     def test_failure_post_with_mismatch_password(self):
 
 
-#class TestLoginView(TestCase):
+# class TestLoginView(TestCase):
 #     def test_success_get(self):
-          
+
 #     def test_success_post(self):
 
 #     def test_failure_post_with_not_exists_user(self):
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from . import views
-from .views import SignupView
 
 app_name = "accounts"
 
