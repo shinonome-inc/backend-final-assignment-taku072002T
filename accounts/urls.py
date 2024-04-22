24f21cf6117b8eb9e10044d125dc1bd0f095e706
taku072002 <taku07200@gmail.com>
2024-04-22 16:41:09 +0900

    Added First Test Contents

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .views import SignupView
 
 app_name = "accounts"
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("accounts.urls"), name="accounts"),
     path("tweets/", include("tweets.urls"), name="tweets"),
     path("", include("welcome.urls"), name="welcome"),
 ]
