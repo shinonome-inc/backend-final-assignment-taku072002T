d2b5e1c93bda013e2ec3a0563a9146f4e61c73a1
taku072002 <taku07200@gmail.com>
2024-04-24 10:15:25 +0900

    Commition for checkout into signup

--- accounts/views.py
+++ accounts/views.py
@@ -4,12 +4,26 @@ from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from .forms import SignupForm
+from .forms import LoginForm, SignupForm
 
 
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
+    success_url = reverse_lazy("tweets:home")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password1"]
+        user = authenticate(self.request, username=username, password=password)
+        login(self.request, user)
+        return response
+
+
+class LoginView(CreateView):
+    form_class = LoginForm
+    template_name = "accounts/login.html"
     success_url = reverse_lazy("tweets:home")
 
     def form_valid(self, form):
