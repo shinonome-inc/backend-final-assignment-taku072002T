59f1a16f02fa76257ee97ca1838264661c1a6f50
taku072002T <taku07200@gmail.com>
2024-06-17 18:20:05 +0900

    Fixed migrations files due to github error

--- tweets/models.py
+++ tweets/models.py
@@ -9,8 +9,8 @@ User = get_user_model()
 
 
 class Tweet(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, blank=True, null=False, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default="")
     title = models.CharField(max_length=50)
 
     content = models.CharField(max_length=100)
