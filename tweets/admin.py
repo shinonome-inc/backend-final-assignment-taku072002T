f96c4ca0406ec010d2f77b7c8d368e636d7a44c6
taku072002T <taku07200@gmail.com>
2024-06-30 21:02:32 +0900

    Edited tweets

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,6 +1,7 @@
 # from django.contrib import adm        in
 from django.contrib import admin
 
-from .models import Tweet
+from .models import Favorite, Tweet
 
 admin.site.register(Tweet)
+admin.site.register(Favorite)
